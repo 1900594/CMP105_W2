91d5800b177676eb762110353d844f432307eeca
1900594 <1900594@uad.ac.uk>
2020-01-23 10:51:18 +0000

    WEEK 2 LAB WORK

--- Week2/CMP105App/Level.cpp
+++ Week2/CMP105App/Level.cpp
@@ -17,7 +17,53 @@ Level::~Level()
 // handle user input
 void Level::handleInput()
 {
+	//keyboard tasks
+	if (input->isKeyDown(sf::Keyboard::W))
+	{
+		input->setKeyUp(sf::Keyboard::W);
+	std::cout << "The W key was pressed!  ";
+	}
 
+	if (input->isKeyDown(sf::Keyboard::J) && input->isKeyDown(sf::Keyboard::K) && input->isKeyDown(sf::Keyboard::L))
+	{
+		input->setKeyUp(sf::Keyboard::J);
+		input->setKeyUp(sf::Keyboard::K);
+		input->setKeyUp(sf::Keyboard::L);
+		std::cout << "J,K and L were pressed\n";
+	}
+
+	if (input->isKeyDown(sf::Keyboard::Escape))
+	{
+		window->close();
+	}
+
+	//mouse tasks
+	if (input->isKeyDown(sf::Keyboard::Q))
+	{
+		//reposition mouse
+		sf::Mouse::setPosition(sf::Vector2i(400, 300), *window);
+
+	}
+
+	
+
+	std::cout << "Mouse X; \n" << input->getMouseX() << "Mouse Y; \n" << input->getMouseY();
+
+
+
+
+	if (!font.loadFromFile("font/arial.ttf")) {
+		std::cout << "Error loading font\n";
+	}
+
+	std::string getMouseX("Mouse X: ");
+	std::string getMouseY("Mouse Y: ");
+	text.setFont(font);
+	text.setCharacterSize(15);
+	text.setString(getMouseX);
+	text.setString(getMouseY);
+	text.setPosition(200, 0);
+	text.setFillColor(sf::Color::Red);
 
 }
 
@@ -31,7 +77,8 @@ void Level::update()
 void Level::render()
 {
 	beginDraw();
-
+	window->draw(text);
+	
 	endDraw();
 }
 
